5cc76c8ee9aaa5332a207d149db8e1dca7c1511e
Agustin Borrego <borrego@us.es>
2021-03-12 14:07:27 +0100

    Initial commit

---
+++ sql/populate_database.sql
@@ -0,0 +1,7 @@
+INSERT INTO Users
+VALUES
+	(1, 'John', 'Doe', '+01 (541) 754-3010', 'john.doe@gallery.com', 'john', 'pbkdf2:sha256:150000$KKgd0xN5$d778b27800d8b89e001843285475a0da3f6f6c664ec8e8a9590ed1c49603b194', '/images/default_profile.png'),
+	(2, 'Jane', 'Smith', '+34 678 387 155', 'jane.doe@gallery.com', 'jane', 'pbkdf2:sha256:150000$v4wgnaXC$b87f5daf437119c21ec712462f4b193b6fada27f485e36502c5cf4553a01f640', '/images/default_profile.png');
+-- Password = username
+
+-- Add some more data for your other tables...
